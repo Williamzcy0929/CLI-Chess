b754a7a87772f76f187dd4d91146ded59b124456
jixiao620 <jl1401@duke.edu>
2026-04-22 02:54:54 +0800

    add marginal case tests

--- src/piece.py
+++ src/piece.py
@@ -2,12 +2,12 @@
 
 from __future__ import annotations
 
-from .pieces.bishop import Bishop
-from .pieces.king import King
-from .pieces.knight import Knight
-from .pieces.pawn import Pawn
-from .pieces.queen import Queen
-from .pieces.rook import Rook
+from pieces.bishop import Bishop
+from pieces.king import King
+from pieces.knight import Knight
+from pieces.pawn import Pawn
+from pieces.queen import Queen
+from pieces.rook import Rook
 
 
 class Piece:
