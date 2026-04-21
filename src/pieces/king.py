bde24742ad1945f14258ad5e520fd45cb637f695
Changyue (William) Zhao <williamzcy929@icloud.com>
2026-04-21 15:23:51 -0400

    fix some ruff errors.

--- src/pieces/king.py
+++ src/pieces/king.py
@@ -10,8 +10,14 @@ class King:
         self.col = col
         self.is_black = is_black
 
-    def is_move_legal(self, board, end_row: int, end_col: int) -> bool:
+    def is_move_legal(self, board: Board, end_row: int, end_col: int) -> bool:
         """Check if the move is legal."""
-        if not board.verify_source_and_destination(self.row, self.col, end_row, end_col, self.is_black):
+        if not board.verify_source_and_destination(
+            self.row,
+            self.col,
+            end_row,
+            end_col,
+            self.is_black,
+        ):
             return False
         return board.verify_adjacent(self.row, self.col, end_row, end_col)
--- src/pieces/knight.py
+++ src/pieces/knight.py
@@ -3,6 +3,7 @@
 from board import Board
 
 class Knight:
+    """Class Knight."""
     def __init__(self, row: int, col: int, is_black: bool) -> None:
         """Initialize the Knight."""
         self.row = row
@@ -13,10 +14,19 @@ class Knight:
         """Verify the knight move."""
         changed_row = abs(end_row - self.row)
         changed_col = abs(end_col - self.col)
-        return (changed_col == 2 and changed_row == 1) or (changed_col == 1 and changed_row == 2)
+        return (
+            (changed_col == 2 and changed_row == 1)
+            or (changed_col == 1 and changed_row == 2)
+        )
 
     def is_move_legal(self, board, end_row: int, end_col: int) -> bool:
         """Check if the move is legal."""
-        if not board.verify_source_and_destination(self.row, self.col, end_row, end_col, self.is_black):
+        if not board.verify_source_and_destination(
+            self.row,
+            self.col,
+            end_row,
+            end_col,
+            self.is_black,
+        ):
             return False
         return self.verify_knight(end_row, end_col)
